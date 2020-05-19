39c0865931a218ec04ac019d7a28f22012edfdfb
Satoshi Kojima <skoji@skoji.jp>
2020-05-19 22:12:56 +0900

    move 菩 to z direction with crank.

--- source/main.lua
+++ source/main.lua
@@ -1,59 +1,33 @@
 import "CoreLibs/graphics"
 import "CoreLibs/sprites"
-import "CoreLibs/timer"
 
 local gfx <const> = playdate.graphics
 local sprite = nil
-local sprite2 = nil
 
 function myGameSetUp()
-   local spriteImage3 = gfx.image.new("images/zai.png")
-   sprite3 = gfx.sprite.new()
-   sprite3:setImage(spriteImage3)
-   sprite3:setCenter(0.5, 0.5)
-   sprite3:moveTo(200,120)
-   sprite3:setZIndex(-10)
-   local spriteImage2 = gfx.image.new("images/ji.png")
-   sprite2 = gfx.sprite.new()
-   sprite2:setImage(spriteImage2)
-   sprite2:setCenter(0.5, 0.5)
-   sprite2:moveTo(200,120)
-   sprite2:setZIndex(0)
-   local spriteImage = gfx.image.new("images/kan.png")
+   local spriteImage = gfx.image.new("images/bo.png")
    sprite = gfx.sprite.new()
    sprite:setImage( spriteImage )
    sprite:setCenter( 0.5, 0.5 )
    sprite:moveTo( 200, 120 )
-   sprite:setZIndex(10)
-   sprite3:add()
-   sprite2:add()
    sprite:add() 
-   
-   
 end
-gfx.setColor(gfx.kColorBlack)
-gfx.fillRect(0,0,400,240)
+
 myGameSetUp()
-playdate.timer:start()
+
+local initialDistance = 400
+local distance = initialDistance
+
 local moveTwo = false
 function playdate.update()
    local change = playdate.getCrankChange()
-   sprite:moveBy(0, change)
-   if moveTwo then
-      sprite2:moveBy(0, change)
+   if change ~= 0 then
+      distance = distance + change * 5
+      sprite:setScale(initialDistance / distance)
+      sprite:setCenter( 0.5, 0.5 )
+      local y = 120 - (distance - initialDistance) * 0.04
+      sprite:moveTo( 200, y )
    end
-   local x, y = sprite:getPosition()
-   if change < 0 and y < -150 then
-      moveTwo = true
-   end
-   if moveTwo then
-      local x2, y2 = sprite2:getPosition()
-      if change > 0 and y2 > 120 then
-         moveTwo = false
-         sprite:moveTo(200,120)
-      end
-   end
-   
    gfx.sprite.update()
-   playdate.timer.updateTimers()
+
 end
