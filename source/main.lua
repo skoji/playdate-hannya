e1daa614e43a158b3bacedc94f33b6362623d259
Satoshi Kojima <skoji@skoji.jp>
2020-05-31 09:21:20 +0900

    add crank mode

--- source/main.lua
+++ source/main.lua
@@ -39,12 +39,38 @@ function myGameSetUp()
 end
 
 myGameSetUp()
+local messageFont = playdate.graphics.getSystemFont(gfx.font.kVariantBold)
+local textHeight = messageFont:getHeight()
+
+function message(text)
+   local textWidth = messageFont:getTextWidth(text)
+   local messageWidth = textWidth + 8
+   local messageHeight = textHeight + 4
+   gfx.drawRect(400 - messageWidth, 240 - messageHeight, messageWidth, messageHeight)
+   messageFont:drawText(text, 400 - textWidth - 4, 240 - textHeight -2)
+end
+
+local mode = "auto"
+
+function playdate.AButtonDown()
+   print(mode)
+   if mode == "auto" then
+      mode = "crank"
+   else
+      mode = "auto"
+   end
+end
 
 function playdate.update()
-   currentChange = 30
-   currentZPosition = currentZPosition + currentChange
+   if mode == "auto" then
+      currentChange = 50
+   else
+      currentChange = playdate.getCrankChange() * 5
+   end
+   currentZPosition = currentZPosition - currentChange
    for k,v in ipairs(vsprites) do
       v:update(currentZPosition)
    end
    gfx.sprite.update()
+   message(mode)
 end
